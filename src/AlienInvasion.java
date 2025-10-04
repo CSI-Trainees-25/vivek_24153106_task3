1c4ae7f8774dc2462bccf89553438d5fbfbbb775
Vivek Yadav <vivekyadav00681@gmail.com>
2025-10-04 17:19:12 +0530

    ...

--- src/AlienInvasion.java
+++ src/AlienInvasion.java
@@ -6,6 +6,25 @@ import javax.swing.*;
 
 
 public class AlienInvasion extends JPanel {
+
+    class Block {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image img;
+        boolean alive = true;
+        boolean used = false;
+
+        Block(int x, int y, int width, int height, Image img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+        }
+    }
+
     int tileSize = 32;
     int rows = 16;
     int columns = 16;
@@ -18,7 +37,16 @@ public class AlienInvasion extends JPanel {
     Image alien3;
     Image alienMonkey;
     Image alienDog;
+    ArrayList<Image> alienImage;
 
+    int shipWidth = tileSize*2;
+    int shipHeight = tileSize;
+    int shipX = tileSize*columns/2 - tileSize;
+    int shipY =  boardHeight - tileSize;
+
+    Block ship;
+
+    Timer gameLoop;
 
     public AlienInvasion(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -30,5 +58,25 @@ public class AlienInvasion extends JPanel {
         alien3 = new ImageIcon(getClass().getResource("./assets/alien3.png")).getImage();
         alienMonkey = new ImageIcon(getClass().getResource("./assets/alienMonkey.png")).getImage();
         alienDog = new ImageIcon(getClass().getResource("./assets/alienDog.png")).getImage();
+
+        alienImage = new ArrayList<>();
+        alienImage.add(alien);
+        alienImage.add(alien2);
+        alienImage.add(alien3);
+        alienImage.add(alienDog);
+        alienImage.add(alienMonkey);
+
+        ship = new Block(shipX, shipY, shipWidth, shipHeight, shipImage);
+
+        gameLoop = new Timer(1000/60, this);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(ship.img, ship.x, ship.y, ship.width, ship.height, null);
     }
 }
